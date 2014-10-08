f803499713ef26c7029c2082215950834f3111eb
Martin Holt Juliussen <martinhj@gmail.com>
2014-10-08 08:33:46 +0200

    Added a populateReadings() function to put in the readings into the readings array history. Removed the millis test that limited the amount of output.

--- LSM303DLHC.ino
+++ LSM303DLHC.ino
@@ -1,3 +1,4 @@
+//https://learn.adafruit.com/lsm303-accelerometer-slash-compass-breakout/calibration
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
@@ -11,14 +12,15 @@ float x = 0;
 const float eGravity = 9.78;
 const float compass = 60.0;
 const int numberOfAxis = 6;
-const int numberOfReadings = 9;
+const int numberOfReadings = 18;
 int index = 0;
-//                              acc x, y z, mag x, y, z:
+//acc x, y z, mag x, y, z:
 float minReadings[numberOfAxis] = 
   {-13.45, -13.18, -10.83, -43.27, -50.0, -56.12};
 float maxReadings[numberOfAxis] = 
   {13.61, 11.96, 11.34, 57.09, 49.73, 52.04};
 float readings[numberOfAxis];
+float readingSums[numberOfAxis];
 float sreadings[numberOfAxis][numberOfReadings];
 unsigned long lastprint = 0;
 
@@ -46,7 +48,7 @@ void loop(void) {
   /*Serial.print((int)lround(event.acceleration.x*100),BIN); Serial.print(" ");*/
   /*Serial.print("xint: "); Serial.print((int)(event.acceleration.x*100)); Serial.print(" ");*/
   //Serial.println(index); 
-  if (millis() - lastprint > 200) {
+  //if (millis() - lastprint > 200) {
     lastprint = millis();
     readings[0] = accevent.acceleration.x;
     readings[1] = accevent.acceleration.y;
@@ -54,6 +56,7 @@ void loop(void) {
     readings[3] = magevent.magnetic.x;
     readings[4] = magevent.magnetic.y;
     readings[5] = magevent.magnetic.z;
+    populateReadings();
 
     Serial.print(map(reading(0), minReadings[0],
           maxReadings[0], -eGravity, eGravity));
@@ -74,10 +77,7 @@ void loop(void) {
     Serial.print(map(reading(5), minReadings[5],
           maxReadings[5], -compass, compass)); 
     Serial.println();
-  }
-  if (index++ >= numberOfReadings - 1) { 
-    index = 0; 
-  }
+  //}
 }
 
 
@@ -85,6 +85,24 @@ float map(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-float reading(int index) {
-  return readings[index];
+float populateReadings() {
+  for (int i = 0; i < numberOfAxis; i++) {
+    sreadings[i][index] = readings[i];
+    if (index == numberOfReadings - 1) {
+      readingSums[i] -= sreadings[i][0];
+    }
+    else {
+      readingSums[i] -= sreadings[i][index + 1];
+    }
+    readingSums[i] += readings[i];
+  }
+  if (index++ >= numberOfReadings - 1) { 
+    index = 0; 
+  }
+}
+
+
+
+float reading(int lindex) {
+  return readingSums[lindex]/numberOfReadings;
 }
